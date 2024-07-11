67f121ecd15b26b3667c5f1dbe503cce47bb4ec9
Дарьяно <dashastepanova1901@mail.ru>
2024-07-11 20:07:58 +0300

    Added all pages and structure

---
+++ src/routes/components/MenuComponent.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let closeMenu: () => void;
+</script>
+
+<div class="dropdown-menu">
+    <div class='h1'>Menu</div>
+
+    <div class="marg"><a href="/map" on:click={closeMenu}>Map робота</a></div>
+    <nav></nav>
+    <div class="marg"><a href="/diagnosis" on:click={closeMenu}>Diagnosis</a></div>
+    <nav></nav>
+    <div class="marg"><a href="/choose" on:click={closeMenu}>Choosing robots</a></div>
+    <nav></nav>
+    <div class="marg"><a href="/settings" on:click={closeMenu}>Robot settings</a></div>
+    <nav></nav>
+    <div class="marg"><a href="/about-us" on:click={closeMenu}>About us</a></div>
+    <div class="marg"><a href="/about-project" on:click={closeMenu}>About project</a></div>
+</div>
+
+<style>
+    /* Add your styles here */
+</style>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Map Page</h1>
+<p>This is the map page.</p>
