9dd716665b2e6b9f233374744e7acfbed91d3fa0
Дарьяно <dashastepanova1901@mail.ru>
2024-07-11 20:22:49 +0300

    Skeleton of the project

---
+++ src/routes/choose/+page.svelte
@@ -0,0 +1,7 @@
+<script></script>
+
+<div class='page-container'>
+    Choosing robots
+    <div class="log-screen">Здесь ничего нет :(</div>
+    <button class="clear-btn">Очистить историю</button>
+</div>
---
+++ src/routes/diagnosis/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    const cameraStatus = "No signal";
+    const wheelsStatus = "No signal";
+    const batteryStatus = "No signal";
+    const current = ""
+</script>
+
+<div class='page-container'>
+    Диагностика робота
+    <div class="diagnostic-id">
+        <div class="diagnostic-text">ID робота:</div>
+        <div class="robot-ID">{current}</div>
+    </div>
+    <div class="diagnostic-element">
+        <div class="diagnostic-text">Камера</div>
+        <div class="bad">{cameraStatus}</div>
+    </div>
+    <div class="diagnostic-element">
+        <div class="diagnostic-text">Колесики</div>
+        <div class="bad">{wheelsStatus}</div>
+    </div>
+    <div class="diagnostic-element">
+        <div class="diagnostic-text">Заряд батареи</div>
+        <div class="bad">{batteryStatus}</div>
+    </div>
+</div>
