33b91911959eba14b798375e6a9b4a26885f8a00
Angel <palkina.sof@gmail.com>
2024-07-12 14:51:29 +0500

    choose

--- src/routes/choose/+page.svelte
+++ src/routes/choose/+page.svelte
@@ -1,7 +1,18 @@
-<script></script>
+<script>
+    import '../../styles/choose.css';
+</script>
 
 <div class='page-container'>
     Choosing robots
-    <div class="log-screen">Здесь ничего нет :(</div>
-    <button class="clear-btn">Очистить историю</button>
+    <div class="all-elements">
+        <button class="element">
+            Robot_1
+        </button>
+        <button class="element">
+            Robot_2
+        </button>
+        <button class="element">
+            Robot_3
+        </button>
+    </div>
 </div>
