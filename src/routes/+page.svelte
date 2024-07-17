3783b334eef317abfa28e463410da42eebdc86b0
Angel <palkina.sof@gmail.com>
2024-07-17 14:55:31 +0500

    fix: update style for page your robots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
     Connect to the robot
     <select class="robotID" id="robotID" name="robotID"
             required>
-        <option value="SomeID1">SomeID1</option>
-        <option value="SomeID2">SomeID2</option>
-        <option value="SomeID3">SomeID3</option>
+        <option  value="SomeID1">SomeID1</option>
+        <option  value="SomeID2">SomeID2</option>
+        <option  value="SomeID3">SomeID3</option>
     </select>
     <button class='connect-btn'>Connect</button>
     <div class="add-txt">Has no robot?</div>
