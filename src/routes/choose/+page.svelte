ae04e8f4c80cc42d4c40e9cd2566a3fafc80671a
Angel <palkina.sof@gmail.com>
2024-07-17 20:46:57 +0500

    fix: add emty elements on click the buttons in your robots

--- src/routes/choose/+page.svelte
+++ src/routes/choose/+page.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import '../../styles/choose.css';
+
+	let isRobotModalOpen: boolean = false;
+
+	function openRobotModal() {
+		isRobotModalOpen = true;
+	}
+
+	function closeRobotModal() {
+		isRobotModalOpen = false;
+	}
 </script>
 
 <head>
@@ -9,11 +19,19 @@
 		content="The page with the list of the robots and info about the Camera, Wheels and Battery charge"
 	/>
 </head>
-<div class="page-container">
+<div class="page-container {isRobotModalOpen ? 'blur' : ''}">
 	Your robots
 	<div class="all-elements">
-		<button class="element"> Robot_1 </button>
-		<button class="element"> Robot_2 </button>
-		<button class="element"> Robot_3 </button>
+		<button class="element" on:click={openRobotModal}> Robot_1 </button>
+		<button class="element" on:click={openRobotModal}> Robot_2 </button>
+		<button class="element" on:click={openRobotModal}> Robot_3 </button>
 	</div>
 </div>
+{#if isRobotModalOpen}
+	<div class="modal-overlay">
+		<div class="modal">
+			<button class="close-button" on:click={closeRobotModal}>X</button>
+			<div class="text-font">Robot</div>
+		</div>
+	</div>
+{/if}
