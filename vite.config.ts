f2f3cb925f98f9882b32da5819f7e73a5f979331
Дарьяно <dashastepanova1901@mail.ru>
2024-07-20 17:07:47 +0300

    read env only from process

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import dotenv from 'dotenv';
-import path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	define: {
-		'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
-	},
 	server: {
 		proxy: {
 			'/api': {
